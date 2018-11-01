d64fdafebb74b3e145ee2bfe2813f9bdb4edbcd5
Irwin <zhd_dong@163.com>
2018-11-01 09:45:53 +0800

    创建项目

---
+++ AddressList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddressList
+//
+//  Created by 会冬 on 2018/10/31.
+//  Copyright © 2018 Irwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddressList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressList
+//
+//  Created by 会冬 on 2018/10/31.
+//  Copyright © 2018 Irwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
